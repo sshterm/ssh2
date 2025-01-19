649102b0afa4b3d9044af75171edaf4648239651
ssh term <admin@ssh2.app>
2025-01-19 21:54:57 +0800

    dev

--- Sources/SSH/Net/MaxMind.swift
+++ Sources/SSH/Net/MaxMind.swift
@@ -9,7 +9,7 @@ public class MaxMind {
     var raw: UnsafeMutablePointer<MMDB_s>?
 
     /// Initializes a new instance of the class with the specified file path.
-    /// 
+    ///
     /// - Parameter file: The path to the MaxMind database file as a `String`.
     /// - Note: The file path is converted to bytes and used to open the MaxMind database.
     public init(file: String) {
@@ -24,7 +24,7 @@ public class MaxMind {
 
 public extension MaxMind {
     /// Looks up the ISO country code for a given IP address.
-    /// 
+    ///
     /// This function checks if the provided IP address is valid and not a local network IP.
     /// If the IP address is valid and not a LAN IP, it queries the MaxMind database for the
     /// corresponding ISO country code.
@@ -41,14 +41,17 @@ public extension MaxMind {
         guard let code = mmdb_lookup_iso_code(raw, ip.bytes) else {
             return nil
         }
+        defer {
+            code.deallocate()
+        }
         return code.string
     }
 
     /// A computed property that provides access to the metadata of the MaxMind database.
-    /// 
+    ///
     /// This property returns an `UnsafeMutablePointer` to an `MMDB_metadata_s` structure,
     /// which contains metadata information about the MaxMind database.
-    /// 
+    ///
     /// - Note: The pointer returned by this property is unsafe and mutable, so it should be
     ///         used with caution to avoid memory safety issues.
     var metadata: UnsafeMutablePointer<MMDB_metadata_s>? {
