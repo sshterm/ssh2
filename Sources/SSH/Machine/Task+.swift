a7f99664af327fa6d43d8ecce2a36a443c8d5395
ssh term <admin@ssh2.app>
2025-02-11 17:55:51 +0800

    dev

--- Sources/SSH/Machine/Task+.swift
+++ Sources/SSH/Machine/Task+.swift
@@ -1,8 +1,11 @@
-//
-//  File.swift
-//  SSH2
-//
-//  Created by 费三量 on 2025/2/11.
-//
+// Task+.swift
+// Copyright (c) 2025 ssh2.app
+// Created by admin@ssh2.app 2025/2/11.
 
 import Foundation
+
+public extension Task where Success == Never, Failure == Never {
+    static func sleep(seconds: Double) async {
+        try? await Task.sleep(for: .seconds(seconds))
+    }
+}
